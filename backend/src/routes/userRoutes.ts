8a754485394b43f116c96265d8acc1431c6a109f
Ani <turboji007@gmail.com>
2025-05-28 21:21:19 +0530

    added basic auth middelware

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,12 +1,13 @@
 import express from 'express';
-import { getCurrentUser, syncUser } from '../controllers/userController';
+import { getCurrentUser, syncUser } from '../controllers/userController.js';
+import {  verifyToken } from '../middleware/authMiddelware.js';
 
 
 
 const router = express.Router();
 
 // Import user controller functions
-router.get('/profile', getCurrentUser);
-router.post("/sync-user", syncUser) 
+router.get('/me', verifyToken, getCurrentUser);
+router.post("/sync-user", verifyToken, syncUser) 
 
 export default router;
