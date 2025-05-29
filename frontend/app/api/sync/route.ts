4f4a271b6df201f84ef7f7861b63413b439b11d8
Ani <turboji007@gmail.com>
2025-05-29 13:45:32 +0530

    added sync route

--- frontend/app/api/sync/route.ts
+++ frontend/app/api/sync/route.ts
@@ -1,44 +1,44 @@
 import { NextResponse } from 'next/server';
-// import { auth0 } from '@/lib/auth0';
+import { auth0 } from '@/lib/auth0';
 
-// return a dummy response for now
-export const GET = async function syncUser() {
-    NextResponse.json({ message: 'Sync user endpoint is under construction' }, { status: 200 });
-}
+// // return a dummy response for now
+// export const GET = async function syncUser() {
+//     NextResponse.json({ message: 'Sync user endpoint is under construction' }, { status: 200 });
+// }
 
 
-// export const POST = async function syncUser() {
-//     try {
-//         const session = await auth0.getSession();
+export const POST = async function syncUser() {
+    try {
+        const session = await auth0.getSession();
     
-//         if (!session) {
-//         return NextResponse.json(
-//             { error: 'Not authenticated' },
-//             { status: 401 }
-//         );
-//         }
+        if (!session) {
+        return NextResponse.json(
+            { error: 'Not authenticated' },
+            { status: 401 }
+        );
+        }
     
-//         const res = new NextResponse();
-//         const { token: accessToken } = await auth0.getAccessToken();
-//         const apiPort = process.env.NEXT_API_URL || 'http://localhost:5000';
-//         const response = await fetch(`${apiPort}/api/user/sync-user`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             Authorization: `Bearer ${accessToken}`
-//         },
-//         body: JSON.stringify({ userId: session.user.sub })
-//         });
+        const res = new NextResponse();
+        const { token: accessToken } = await auth0.getAccessToken();
+        const apiPort = process.env.NEXT_API_URL || 'http://localhost:5000';
+        const response = await fetch(`${apiPort}/api/user/sync-user`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`
+        },
+        body: JSON.stringify({ userId: session.user.sub })
+        });
     
-//         if (!response.ok) {
-//         throw new Error('Failed to sync user');
-//         }
+        if (!response.ok) {
+        throw new Error('Failed to sync user');
+        }
     
-//         const data = await response.json();
-//         return NextResponse.json(data, res);
-//     } catch (error: unknown) {
-//         const errorMessage = error instanceof Error ? error.message : 'Unknown error occurred';
-//         const errorStatus = (error as { status?: number }).status || 500;
-//         return NextResponse.json({ error: errorMessage }, { status: errorStatus });
-//     }
-//     }
+        const data = await response.json();
+        return NextResponse.json(data, res);
+    } catch (error: unknown) {
+        const errorMessage = error instanceof Error ? error.message : 'Unknown error occurred';
+        const errorStatus = (error as { status?: number }).status || 500;
+        return NextResponse.json({ error: errorMessage }, { status: errorStatus });
+    }
+    }
