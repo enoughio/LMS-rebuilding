16cdb0c92a6e8828ab17c9a9926bd3e1e9850685
Ani <turboji007@gmail.com>
2025-05-29 13:26:33 +0530

    ad

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import EqualSizedBoxes from "@/components/story1";
-import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 export default function Story() {
     return (
