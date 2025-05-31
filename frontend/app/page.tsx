78db42f981b13e45ed2994e86f20c5b9356d70ec
joohirao <joohirao0413@gmail.com>
2025-05-31 18:53:05 +0530

    new

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,7 +7,7 @@ import Ready from "@/components/home/readyToJoin";
 export default function App() {
   return (
     <>
-      <main className=" max-w-[1920px]">
+      <main className=" max-w-[1920px] min-h-screen mx-auto xl:mx-0">
         
           <Hero />
           <What />
