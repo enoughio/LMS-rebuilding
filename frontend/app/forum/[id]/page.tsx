85d1a9e4872bd2047bb085efeb94c8bb1849ccf0
joohirao <joohirao0413@gmail.com>
2025-06-07 16:10:05 +0530

    FORUM PAGE - width fixed

--- frontend/app/forum/[id]/page.tsx
+++ frontend/app/forum/[id]/page.tsx
@@ -10,7 +10,7 @@ export default async function Page(
   const { id } = await params;      
 
   return (
-    <div className="flex flex-col items-center justify-center w-full bg-[#ECE3DA] pb-20">
+    <div className="flex flex-col  w-[99vw] overflow-x-hidden min-h-screen bg-[#ECE3DA] pb-20">
       <ForumPostPage />
       <CommentsSection postId={id} />
     </div>
